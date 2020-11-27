6b80f82c215e9d71ce3d2f754c7391e364706c7b
Oussama Bougaila <oussama.bougaila@gmail.com>
2020-11-27 13:09:48 +0100

    feat: Add new pipe PokemonTypeColor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListPokemonComponent } from './pokemons/list-pokemon/list-pokemon.component';
 import { BorderCardDirective } from './shared/directives/border-card.directive';
+import { PokemonTypeColorPipe } from './shared/pipes/pokemon-type-color.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     ListPokemonComponent,
     BorderCardDirective,
+    PokemonTypeColorPipe,
   ],
   imports: [
     BrowserModule,
