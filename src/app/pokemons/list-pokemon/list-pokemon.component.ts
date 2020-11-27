08ce96ed69c4a05e8b54ba209b2eec1e8c233ade
Oussama Bougaila <oussama.bougaila@gmail.com>
2020-11-27 13:01:32 +0100

    feat: Add new component ListPokemon

---
+++ src/app/pokemons/list-pokemon/list-pokemon.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Pokemon } from '../../pokemon';
+import { POKEMONS } from '../../shared/list.pokemons';
+
+@Component({
+  selector: 'app-list-pokemon',
+  templateUrl: './list-pokemon.component.html',
+  styleUrls: ['./list-pokemon.component.scss']
+})
+export class ListPokemonComponent implements OnInit {
+
+  pokemons: Pokemon[] = POKEMONS;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
