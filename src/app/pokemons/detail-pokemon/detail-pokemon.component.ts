b6f58aea785e3076c87db6950b00ec1585117597
Oussama Bougaila <oussama.bougaila@gmail.com>
2020-12-08 05:42:01 +0100

    feat: Update pokemon detail

--- src/app/pokemons/detail-pokemon/detail-pokemon.component.ts
+++ src/app/pokemons/detail-pokemon/detail-pokemon.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {Pokemon} from '../../pokemon';
+import {ActivatedRoute, Router} from '@angular/router';
+import {POKEMONS} from '../../shared/list.pokemons';
 
 @Component({
   selector: 'app-detail-pokemon',
@@ -7,9 +10,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailPokemonComponent implements OnInit {
 
-  constructor() { }
+  listOfPokemons: Pokemon[] = POKEMONS;
+  pokemonToDisplay: Pokemon = {};
+
+  constructor(private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.pokemonToDisplay = this.getPokemon(Number(id));
+    console.log(this.pokemonToDisplay);
+  }
+
+  goBack(): void {
+    this.router.navigate(['/']);
+  }
+
+  getPokemon(id: number): any {
+    let pokemon = null;
+    const size = this.listOfPokemons.length;
+    let i = 0;
+    let found = false;
+    while (i < size && !found){
+      if (this.listOfPokemons[i].id === id){
+        found = true;
+        pokemon = this.listOfPokemons[i];
+      }
+      i++;
+    }
+    return pokemon;
   }
 
 }
